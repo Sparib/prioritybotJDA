858cbaa0d1a508c608772fa179ace6a6c2522cf0
Sparib <52337413+Sparib@users.noreply.github.com>
2020-10-25 20:40:48 -0400

    Added license

--- src/main/java/sparib/prioritybot/main/Bot.java
+++ src/main/java/sparib/prioritybot/main/Bot.java
@@ -1,3 +1,9 @@
+/*
+    Except where otherwise noted, all of the work under the src/main folder
+    is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+    To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+
 package sparib.prioritybot.main;
 
 import io.github.cdimascio.dotenv.Dotenv;
